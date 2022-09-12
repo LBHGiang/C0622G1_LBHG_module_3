87b98396fedd02415c635dab85797ff18840b315
LBHGiang <hgno3001@gmail.com>
2022-09-12 15:00:28 +0700

    edit bai tap bai 1

--- database_module_3/ss1_database_system/student_management.sql
+++ database_module_3/ss1_database_system/student_management.sql
@@ -1,19 +1,22 @@
-drop database if exists teacherstudent_management;
+drop database if exists student_management;
 create database student_management;
 use student_management;
+
 create table teacher (
-`ma_giao_vien` int primary key not null,
-`ten_giao_vien` varchar(45),
-`tuoi` int,
-`ten_nuoc` varchar(45)
+ma_giao_vien int primary key not null,
+ten_giao_vien varchar(45),
+tuoi int,
+ten_nuoc varchar(45)
 );
+
 create table student (
-`ma_hoc_sinh` int primary key not null,
-`ten_hoc_sinh` varchar(45),
-`tuoi` int,
-`ten_nuoc` varchar(45)
+ma_hoc_sinh int primary key not null,
+ten_hoc_sinh varchar(45),
+tuoi int,
+ten_nuoc varchar(45)
 );
+
 create table class (
-`ma_lop` int primary key not null,
-`ten_lop` varchar(45)
+ma_lop int primary key not null,
+ten_lop varchar(45)
 );
