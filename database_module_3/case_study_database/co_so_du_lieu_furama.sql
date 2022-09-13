26fe89fdb71f16d16e9ab8d58324d24681b7219f
LBHGiang <hgno3001@gmail.com>
2022-09-13 21:09:52 +0700

    up case study task 3

--- database_module_3/case_study_database/co_so_du_lieu_furama.sql
+++ database_module_3/case_study_database/co_so_du_lieu_furama.sql
@@ -4,18 +4,18 @@ use co_so_du_lieu_furama;
 
 -- Tạo bảng thuộc tính của nhân viên --
 create table vi_tri (
-ma_vi_tri int primary key,
+ma_vi_tri int primary key auto_increment,
 ten_vi_tri varchar(45));
 create table trinh_do (
-ma_trinh_do int primary key,
+ma_trinh_do int primary key auto_increment,
 ten_trinh_do varchar(45));
 create table bo_phan (
-ma_bo_phan int primary key,
+ma_bo_phan int primary key auto_increment,
 ten_bo_phan varchar(45));
 
 -- Tạo bảng nhân viên--
 create table nhan_vien (
-ma_nhan_vien int primary key,
+ma_nhan_vien int primary key auto_increment,
 ho_ten varchar(45),
 ngay_sinh Date,
 so_cmnd varchar(45),
@@ -33,10 +33,10 @@ foreign key (ma_bo_phan) references bo_phan(ma_bo_phan)
 
 -- Tạo bảng khách hàng và loại khách --
 create table loai_khach (
-ma_loai_khach int primary key,
+ma_loai_khach int primary key auto_increment,
 ten_loai_khach varchar(45));
 create table khach_hang (
-ma_khach_hang int primary key,
+ma_khach_hang int primary key auto_increment,
 ma_loai_khach int,
 ho_ten varchar(45),
 ngay_sinh Date,
@@ -50,13 +50,13 @@ foreign key (ma_loai_khach) references loai_khach(ma_loai_khach)
 
 -- 	Tạo bảng dịch vụ --
 create table loai_dich_vu(
-ma_loai_dich_vu int primary key,
+ma_loai_dich_vu int primary key auto_increment,
 ten_loai_dich_vu varchar(45));
 create table kieu_thue (
-ma_kieu_thue int primary key,
+ma_kieu_thue int primary key auto_increment,
 ten_kieu_thue varchar(45));
 create table dich_vu (
-ma_dich_vu int primary key,
+ma_dich_vu int primary key auto_increment,
 ten_dich_vu varchar(45),
 dien_tich int,
 chi_phi_thue double,
@@ -74,7 +74,7 @@ foreign key (ma_kieu_thue) references kieu_thue(ma_kieu_thue)
 
 -- Tạo bảng hợp đồng --
 create table hop_dong (
-ma_hop_dong int primary key,
+ma_hop_dong int primary key auto_increment,
 ngay_lam_hop_dong Datetime,
 ngay_ket_thuc Datetime,
 tien_dat_coc double,
@@ -88,7 +88,7 @@ foreign key (ma_dich_vu) references dich_vu(ma_dich_vu)
 
 -- Tạo bảng dịch vụ đi kèm --
 create table dich_vu_di_kem (
-ma_dich_vu_di_kem int primary key,
+ma_dich_vu_di_kem int primary key auto_increment,
 ten_dich_vu_di_kem varchar(45),
 gia double,
 don_vi varchar(45),
@@ -96,7 +96,7 @@ trang_thai varchar(45));
 
 -- Tạo bảng hợp đồng chi tiết --
 create table hop_dong_chi_tiet (
-ma_hop_dong_chi_tiet int primary key,
+ma_hop_dong_chi_tiet int primary key auto_increment,
 ma_hop_dong int,
 ma_dich_vu_di_kem int,
 so_luong int,
---
+++ database_module_3/case_study_database/thao_tac_table.sql
@@ -0,0 +1,3 @@
+use co_so_du_lieu_furama;
+SET SQL_SAFE_UPDATES = 0;
+delete from nhan_vien;
